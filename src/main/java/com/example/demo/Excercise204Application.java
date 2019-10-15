2ae258aa4cf4ece70ffba132c3f881ed54d239bd
GBTC440005UR <GBTC440005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-14 20:28:00 -0400

    First commit.

---
+++ src/main/java/com/example/demo/Excercise204Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excercise204Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Excercise204Application.class, args);
+    }
+
+}
